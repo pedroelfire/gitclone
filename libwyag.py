4e52353a123c936ec176cd60f0323f88bb3e0e9c
Pedroelfire <pedroelfire@gmail.com>
2024-09-09 20:53:23 -0600

    first commit

---
+++ libwyag.py
@@ -0,0 +1,12 @@
+import argparse
+import collections
+import configparser
+from datetime import datetime
+import grp, pwd
+from fnmatch import fnmatch
+import hashlib
+from math import ceil
+import os
+import re
+import sys
+import zlib
